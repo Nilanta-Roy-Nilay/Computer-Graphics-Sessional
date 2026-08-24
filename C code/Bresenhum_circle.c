a0577db2d87b81db52220668ac323b5a9cc9f0bd
Nilanta Roy Nilay <156948038+Nilanta-Roy-Nilay@users.noreply.github.com>
2026-08-24 16:18:58 +0600

    Add files via upload

---
+++ C code/Bresenhum_circle.c
@@ -0,0 +1,50 @@
+#include <stdio.h>
+#include <graphics.h>
+#include <math.h>
+
+int main() {
+    int gd = DETECT, gm;
+    int x, y, xc, yc, r, p, xmax, ymax;
+
+    printf("Input center of the circle (xc yc): ");
+    scanf("%d%d", &xc, &yc);
+
+    printf("Input the radius of the circle (r): ");
+    scanf("%d", &r);
+
+    initgraph(&gd, &gm, "");
+
+    xmax = getmaxx();
+    ymax = getmaxy();
+
+    line(xmax / 2, 0, xmax / 2, ymax);
+    line(0, ymax / 2, xmax, ymax / 2);
+
+    p = 3 - (2 * r);
+    x = 0;
+    y = r;
+
+    while (x <= y) {
+        putpixel((xmax / 2 + xc + x), (ymax / 2 - yc + y), WHITE);
+        putpixel((xmax / 2 + xc - x), (ymax / 2 - yc + y), WHITE);
+        putpixel((xmax / 2 + xc + x), (ymax / 2 - yc - y), WHITE);
+        putpixel((xmax / 2 + xc - x), (ymax / 2 - yc - y), WHITE);
+        putpixel((xmax / 2 + xc + y), (ymax / 2 - yc + x), WHITE);
+        putpixel((xmax / 2 + xc - y), (ymax / 2 - yc + x), WHITE);
+        putpixel((xmax / 2 + xc + y), (ymax / 2 - yc - x), WHITE);
+        putpixel((xmax / 2 + xc - y), (ymax / 2 - yc - x), WHITE);
+
+        if (p < 0) {
+            x = x + 1;
+            p = p + (4 * x) + 6;
+        } else {
+            x = x + 1;
+            y = y - 1;
+            p = p + (4 * (x - y)) + 10;
+        }
+    }
+
+    getch();
+    closegraph();
+    return 0;
+}
