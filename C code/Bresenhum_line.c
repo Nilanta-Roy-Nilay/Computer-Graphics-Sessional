174d7e9496d29ac127f6d1a1efe0688abb6f2697
Nilanta Roy Nilay <156948038+Nilanta-Roy-Nilay@users.noreply.github.com>
2026-08-30 00:40:54 +0600

    Update Bresenhum_line.c

--- C code/Bresenhum_line.c
+++ C code/Bresenhum_line.c
@@ -1,44 +1,29 @@
 #include <stdio.h>
-#include <conio.h>
 #include <graphics.h>
 
 int main() {
-    int x, y, x1, y1, x2, y2, dx, dy, p, i;
-    int xmax, ymax;
+    int x1, y1, x2, y2;
+    printf("Enter first point: ");
+    scanf("%d %d", &x1, &y1);
+    printf("Enter second point: ");
+    scanf("%d %d", &x2, &y2);
+
     int gd = DETECT, gm;
-
-    printf("Enter first point : ");
-    scanf("%d%d", &x1, &y1);
-
-    printf("Enter second point : ");
-    scanf("%d%d", &x2, &y2);
-
     initgraph(&gd, &gm, "");
 
-    xmax = getmaxx();
-    ymax = getmaxy();
+    int dx = x2 - x1;
+    int dy = y2 - y1;
+    int p = 2 * dy - dx;
+    int x = x1, y = y1;
 
-    line(xmax / 2, 0, xmax / 2, ymax);
-    line(0, ymax / 2, xmax, ymax / 2);
-
-    dx = x2 - x1;
-    dy = y2 - y1;
-    p = 2 * dy - dx;
-
-    x = x1;
-    y = y1;
-    i = 0;
-
-    while (x < x2) {
-        putpixel((xmax / 2 + x), (ymax / 2 - y), WHITE);
-        
+    while (x <= x2) {
+        putpixel(x, y, WHITE);
+        x++;
         if (p < 0) {
-            x = x + 1;
-            p = p + 2 * dy;
+            p += 2 * dy;
         } else {
-            x = x + 1;
-            y = y + 1;
-            p = p + 2 * dy - 2 * dx;
+            y++;
+            p += 2 * (dy - dx);
         }
     }
 
