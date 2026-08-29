6c18816c996e0f79646b3272dcb4323643491f63
Nilanta Roy Nilay <156948038+Nilanta-Roy-Nilay@users.noreply.github.com>
2026-08-30 00:37:01 +0600

    Update DDA_line.c

--- C code/DDA_line.c
+++ C code/DDA_line.c
@@ -3,46 +3,37 @@
 #include <math.h>
 
 int main() {
-    float x1, y1, x2, y2, i, xi, yi;
-    float x, y, dx, dy, length;
-    int xmax, ymax;
-    int gd = DETECT, gm;
+    float x1, y1, x2, y2, dx, dy, length, xi, yi, x, y;
+    int i, gd = DETECT, gm;
 
-    printf("Enter first point : ");
-    scanf("%f%f", &x1, &y1);
-
-    printf("Enter second point : ");
-    scanf("%f%f", &x2, &y2);
+    printf("Enter first point: ");
+    scanf("%f %f", &x1, &y1);
+    printf("Enter second point: ");
+    scanf("%f %f", &x2, &y2);
 
     initgraph(&gd, &gm, "");
+    int xmax = getmaxx(), ymax = getmaxy();
 
-    xmax = getmaxx();
-    ymax = getmaxy();
-
-    line(xmax / 2, 0, xmax / 2, ymax);
-    line(0, ymax / 2, xmax, ymax / 2);
+    // axes
+    line(xmax/2, 0, xmax/2, ymax);
+    line(0, ymax/2, xmax, ymax/2);
 
     dx = x2 - x1;
     dy = y2 - y1;
 
-    if (abs(dx) >= abs(dy)) {
-        length = abs(dx);
-    } else {
-        length = abs(dy);
-    }
+    if (abs((int)dx) >= abs((int)dy))
+        length = abs((int)dx);
+    else
+        length = abs((int)dy);
 
     xi = dx / length;
     yi = dy / length;
 
-    x = x1;
-    y = y1;
-    i = 0;
+    x = x1; y = y1;
 
-    while (i <= length) {
-        putpixel((xmax / 2 + x), (ymax / 2 - y), WHITE);
-        x = x + xi;
-        y = y + yi;
-        i++;
+    for (i = 0; i <= length; i++) {
+        putpixel(xmax/2 + (int)(x+0.5), ymax/2 - (int)(y+0.5), WHITE);
+        x += xi; y += yi;
     }
 
     getch();
