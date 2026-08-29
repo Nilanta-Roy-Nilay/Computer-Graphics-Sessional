5df94e17f56e565d382a0308a7187e9e38ee62d6
Nilanta Roy Nilay <156948038+Nilanta-Roy-Nilay@users.noreply.github.com>
2026-08-30 01:39:26 +0600

    Update dda_algorithm.cpp

--- Cpp code/dda_algorithm.cpp
+++ Cpp code/dda_algorithm.cpp
@@ -1,38 +1,26 @@
-#include <iostream>
 #include <graphics.h>
+#include <conio.h>
+#include <iostream>
 #include <cmath>
 using namespace std;
 
 int main() {
-    float x1, y1, x2, y2;
-    cout << "Enter first point: ";
-    cin >> x1 >> y1;
-    cout << "Enter second point: ";
-    cin >> x2 >> y2;
+    int gd=DETECT,gm;
+    initgraph(&gd,&gm,"");
 
-    int gd = DETECT, gm;
-    initgraph(&gd, &gm, "");
-    int xmax = getmaxx(), ymax = getmaxy();
+    int x1,y1,x2,y2;
+    cout<<"Enter x1 y1: "; cin>>x1>>y1;
+    cout<<"Enter x2 y2: "; cin>>x2>>y2;
 
-    line(xmax/2, 0, xmax/2, ymax);
-    line(0, ymax/2, xmax, ymax/2);
+    int dx=x2-x1, dy=y2-y1;
+    int steps = (abs(dx)>=abs(dy))?abs(dx):abs(dy);
 
-    float dx = x2 - x1;
-    float dy = y2 - y1;
+    float xi=(float)dx/steps, yi=(float)dy/steps;
+    float x=x1, y=y1;
 
-    float length;
-    if (abs((int)dx) >= abs((int)dy))
-        length = abs((int)dx);
-    else
-        length = abs((int)dy);
-
-    float xi = dx / length;
-    float yi = dy / length;
-
-    float x = x1, y = y1;
-    for (int i = 0; i <= length; i++) {
-        putpixel(xmax/2 + (int)(x+0.5), ymax/2 - (int)(y+0.5), WHITE);
-        x += xi; y += yi;
+    for(int i=0;i<=steps;i++) {
+        putpixel((int)(x+0.5),(int)(y+0.5),WHITE);
+        x+=xi; y+=yi;
     }
 
     getch();
