822a7ab7add9693cf84988f6233c7c621d4be791
Nilanta Roy Nilay <156948038+Nilanta-Roy-Nilay@users.noreply.github.com>
2026-08-30 01:39:04 +0600

    Update brasenhams_line_algorithm.cpp

--- Cpp code/brasenhams_line_algorithm.cpp
+++ Cpp code/brasenhams_line_algorithm.cpp
@@ -1,31 +1,25 @@
-#include <iostream>
 #include <graphics.h>
+#include <conio.h>
+#include <iostream>
 using namespace std;
 
 int main() {
-    int x1, y1, x2, y2;
-    cout << "Enter first point: ";
-    cin >> x1 >> y1;
-    cout << "Enter second point: ";
-    cin >> x2 >> y2;
+    int gd=DETECT,gm;
+    initgraph(&gd,&gm,"");
 
-    int gd = DETECT, gm;
-    initgraph(&gd, &gm, "");
+    int x1,y1,x2,y2;
+    cout<<"Enter x1 y1: "; cin>>x1>>y1;
+    cout<<"Enter x2 y2: "; cin>>x2>>y2;
 
-    int dx = x2 - x1;
-    int dy = y2 - y1;
-    int p = 2 * dy - dx;
-    int x = x1, y = y1;
+    int dx=x2-x1, dy=y2-y1;
+    int p=2*dy-dx;
+    int x=x1,y=y1;
 
-    while (x <= x2) {
-        putpixel(x, y, WHITE);
+    while(x<=x2) {
+        putpixel(x,y,WHITE);
+        if(p<0) p+=2*dy;
+        else { y++; p+=2*(dy-dx); }
         x++;
-        if (p < 0) {
-            p += 2 * dy;
-        } else {
-            y++;
-            p += 2 * (dy - dx);
-        }
     }
 
     getch();
