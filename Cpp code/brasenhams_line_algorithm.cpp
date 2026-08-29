1bf5cd2d25f832d5fa80dfd5a4845978cd7d183d
Nilanta Roy Nilay <156948038+Nilanta-Roy-Nilay@users.noreply.github.com>
2026-08-30 00:43:42 +0600

    Update brasenhams_line_algorithm.cpp

--- Cpp code/brasenhams_line_algorithm.cpp
+++ Cpp code/brasenhams_line_algorithm.cpp
@@ -1,29 +1,34 @@
-#include <graphics.h>
 #include <iostream>
+#include <graphics.h>
 using namespace std;
 
 int main() {
-    int gd = DETECT, gm;
-    initwindow(800, 600, (char*)"Bresenham Line Drawing");
-
     int x1, y1, x2, y2;
-    cout << "Enter line coordinates (x1 y1 x2 y2): ";
-    cin >> x1 >> y1 >> x2 >> y2;
+    cout << "Enter first point: ";
+    cin >> x1 >> y1;
+    cout << "Enter second point: ";
+    cin >> x2 >> y2;
 
-    int dx = abs(x2 - x1), dy = abs(y2 - y1);
-    int sx = (x1 < x2) ? 1 : -1;
-    int sy = (y1 < y2) ? 1 : -1;
-    int err = dx - dy;
+    int gd = DETECT, gm;
+    initgraph(&gd, &gm, "");
 
-    while (true) {
-        putpixel(x1, y1, WHITE);
-        if (x1 == x2 && y1 == y2) break;
-        int e2 = 2 * err;
-        if (e2 > -dy) { err -= dy; x1 += sx; }
-        if (e2 < dx) { err += dx; y1 += sy; }
+    int dx = x2 - x1;
+    int dy = y2 - y1;
+    int p = 2 * dy - dx;
+    int x = x1, y = y1;
+
+    while (x <= x2) {
+        putpixel(x, y, WHITE);
+        x++;
+        if (p < 0) {
+            p += 2 * dy;
+        } else {
+            y++;
+            p += 2 * (dy - dx);
+        }
     }
 
-    outtextxy(50, 30, (char*)"Bresenham Line");
     getch();
     closegraph();
+    return 0;
 }
