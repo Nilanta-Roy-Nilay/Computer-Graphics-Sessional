e3d64ab0b737523c3ab1ec34ffacdbba8de2282e
Nilanta Roy Nilay <156948038+Nilanta-Roy-Nilay@users.noreply.github.com>
2026-08-30 01:39:55 +0600

    Update direct_line.cpp

--- Cpp code/direct_line.cpp
+++ Cpp code/direct_line.cpp
@@ -1,65 +1,22 @@
-#include <iostream>
 #include <graphics.h>
 #include <conio.h>
+#include <iostream>
+using namespace std;
 
-int main()
-{
-    int gd = DETECT, gm;
-    initgraph(&gd, &gm, "");
+int main() {
+    int gd=DETECT,gm;
+    initgraph(&gd,&gm,"");
 
-    int x1, y1, x2, y2;
-    int x, xend;
-    float y, dx, dy, m, c;
+    int x1,y1,x2,y2;
+    cout<<"Enter x1 y1: "; cin>>x1>>y1;
+    cout<<"Enter x2 y2: "; cin>>x2>>y2;
 
-    std::cout << "Enter the value of x1: ";
-    std::cin >> x1;
+    float m=(float)(y2-y1)/(x2-x1);
+    float c=y1-m*x1;
 
-    std::cout << "Enter the value of y1: ";
-    std::cin >> y1;
-
-    std::cout << "Enter the value of x2: ";
-    std::cin >> x2;
-
-    std::cout << "Enter the value of y2: ";
-    std::cin >> y2;
-
-    dx = (float)(x2 - x1);
-    dy = (float)(y2 - y1);
-
-    // handle vertical line to avoid division by zero
-    if (dx == 0.0f) {
-        int ymin = (y1 < y2) ? y1 : y2;
-        int ymax = (y1 > y2) ? y1 : y2;
-        for (int yy = ymin; yy <= ymax; yy++) {
-            putpixel(x1, yy, WHITE);
-        }
-    } else {
-        m = dy / dx;
-
-        if (dx > 0)
-        {
-            x = x1;
-            y = (float)y1;
-            xend = x2;
-        }
-        else
-        {
-            x = x2;
-            y = (float)y2;
-            xend = x1;
-        }
-
-        c = y - m * x;
-
-        while (x != xend)
-        {
-            putpixel(x, (int)(y + 0.5f), WHITE);
-
-            x = x + 1;
-            y = m * x + c;
-        }
-
-        putpixel(xend, (int)(y + 0.5f), WHITE);
+    for(int x=x1;x<=x2;x++) {
+        int y=(int)(m*x+c+0.5);
+        putpixel(x,y,WHITE);
     }
 
     getch();
