15fa34d7070db955593bed0bfb793571396d1f10
Nilanta Roy Nilay <156948038+Nilanta-Roy-Nilay@users.noreply.github.com>
2026-08-31 23:26:10 +0600

    Update 2d_transformation.c

--- C code/2d_transformation.c
+++ C code/2d_transformation.c
@@ -1,174 +1,76 @@
 #include <graphics.h>
 #include <conio.h>
-
-int main()
-{
-    int gd = DETECT, gm;
-    initgraph(&gd, &gm, "");
-
-    rectangle(100, 100, 200, 200);
-
-    int tx = 100;
-    int ty = 50;
-
-    rectangle(100 + tx, 100 + ty,
-              200 + tx, 200 + ty);
-
-    getch();
-    closegraph();
-
-    return 0;
-}
-
-
-
-
-rota
-
-#include <graphics.h>
-#include <conio.h>
+#include <stdio.h>
 #include <math.h>
 
-int main()
-{
+void drawPolygon(int x1, int y1, int x2, int y2, int x3, int y3, int x4, int y4) {
+    line(x1, y1, x2, y2);
+    line(x2, y2, x3, y3);
+    line(x3, y3, x4, y4);
+    line(x4, y4, x1, y1);
+}
+
+int main() {
     int gd = DETECT, gm;
     initgraph(&gd, &gm, "");
 
+    // Original Rectangle Coordinates
     int x1 = 100, y1 = 100;
     int x2 = 200, y2 = 100;
     int x3 = 200, y3 = 200;
     int x4 = 100, y4 = 200;
 
-    float angle = 12;
-    float rad = angle * 3.1416 / 180;
+    int choice;
+    printf("--- 2D Transformations Menu ---\n");
+    printf("1. Translation\n");
+    printf("2. Rotation\n");
+    printf("3. Scaling\n");
+    printf("4. Reflection (X-axis)\n");
+    printf("5. Shearing (X-axis)\n");
+    printf("Enter choice: ");
+    scanf("%d", &choice);
 
-    int nx1, ny1, nx2, ny2;
-    int nx3, ny3, nx4, ny4;
-
-    // Original
+    // Original Shape
     rectangle(x1, y1, x3, y3);
 
-    // Rotation around origin
-    nx1 = x1 * cos(rad) - y1 * sin(rad);
-    ny1 = x1 * sin(rad) + y1 * cos(rad);
+    if (choice == 1) { // Translation
+        int tx = 100, ty = 50;
+        rectangle(x1 + tx, y1 + ty, x3 + tx, y3 + ty);
+    } 
+    else if (choice == 2) { // Rotation
+        float angle = 12;
+        float rad = angle * 3.1416 / 180;
 
-    nx2 = x2 * cos(rad) - y2 * sin(rad);
-    ny2 = x2 * sin(rad) + y2 * cos(rad);
+        int nx1 = x1 * cos(rad) - y1 * sin(rad);
+        int ny1 = x1 * sin(rad) + y1 * cos(rad);
+        int nx2 = x2 * cos(rad) - y2 * sin(rad);
+        int ny2 = x2 * sin(rad) + y2 * cos(rad);
+        int nx3 = x3 * cos(rad) - y3 * sin(rad);
+        int ny3 = x3 * sin(rad) + y3 * cos(rad);
+        int nx4 = x4 * cos(rad) - y4 * sin(rad);
+        int ny4 = x4 * sin(rad) + y4 * cos(rad);
 
-    nx3 = x3 * cos(rad) - y3 * sin(rad);
-    ny3 = x3 * sin(rad) + y3 * cos(rad);
+        drawPolygon(nx1, ny1, nx2, ny2, nx3, ny3, nx4, ny4);
+    } 
+    else if (choice == 3) { // Scaling
+        float sx = 1.5, sy = 2.0;
+        rectangle(x1 * sx, y1 * sy, x3 * sx, y3 * sy);
+    } 
+    else if (choice == 4) { // Reflection
+        // Screen center adjustment for Reflection visual
+        rectangle(x1, -y1 + 300, x3, -y3 + 300);
+    } 
+    else if (choice == 5) { // Shearing
+        int shx = 1;
+        int nx1 = x1 + shx * y1;
+        int nx2 = x2 + shx * y2;
+        int nx3 = x3 + shx * y3;
+        int nx4 = x4 + shx * y4;
 
-    nx4 = x4 * cos(rad) - y4 * sin(rad);
-    ny4 = x4 * sin(rad) + y4 * cos(rad);
-
-    line(nx1, ny1, nx2, ny2);
-    line(nx2, ny2, nx3, ny3);
-    line(nx3, ny3, nx4, ny4);
-    line(nx4, ny4, nx1, ny1);
-
-    getch();
-    closegraph();
-
-    return 0;
-}
-
-
-
-
-
-Scaling
-
-#include <graphics.h>
-#include <conio.h>
-
-int main()
-{
-    int gd = DETECT, gm;
-    initgraph(&gd, &gm, "");
-
-    int x1 = 100, y1 = 100;
-    int x2 = 200, y2 = 200;
-
-    float sx = 1.5;
-    float sy = 2.0;
-
-    // Original
-    rectangle(x1, y1, x2, y2);
-
-    // Scaling
-    rectangle(x1 * sx, y1 * sy,
-              x2 * sx, y2 * sy);
+        drawPolygon(nx1, y1, nx2, y2, nx3, y3, nx4, y4);
+    }
 
     getch();
     closegraph();
-
-    return 0;
-}
-
-
-
-
-Reflection
-
-#include <graphics.h>
-#include <conio.h>
-
-int main()
-{
-    int gd = DETECT, gm;
-    initgraph(&gd, &gm, "");
-
-    int x1 = 100, y1 = 100;
-    int x2 = 200, y2 = 200;
-
-    // Original
-    rectangle(x1, y1, x2, y2);
-
-    // Reflection about X-axis
-    rectangle(x1, -y1, x2, -y2);
-
-    getch();
-    closegraph();
-
-    return 0;
-}
-
-
-
-
-Shearing
-
-#include <graphics.h>
-#include <conio.h>
-
-int main()
-{
-    int gd = DETECT, gm;
-    initgraph(&gd, &gm, "");
-
-    int x1 = 100, y1 = 100;
-    int x2 = 200, y2 = 100;
-    int x3 = 200, y3 = 200;
-    int x4 = 100, y4 = 200;
-
-    int shx = 1;
-
-    // Original
-    rectangle(x1, y1, x3, y3);
-
-    // Shearing
-    int nx1 = x1 + shx * y1;
-    int nx2 = x2 + shx * y2;
-    int nx3 = x3 + shx * y3;
-    int nx4 = x4 + shx * y4;
-
-    line(nx1, y1, nx2, y2);
-    line(nx2, y2, nx3, y3);
-    line(nx3, y3, nx4, y4);
-    line(nx4, y4, nx1, y1);
-
-    getch();
-    closegraph();
-
     return 0;
 }
