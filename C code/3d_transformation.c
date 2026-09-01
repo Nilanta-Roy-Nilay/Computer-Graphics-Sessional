3d7c306322586de12a4c2fb4e3af997642b3e918
Nilanta Roy Nilay <156948038+Nilanta-Roy-Nilay@users.noreply.github.com>
2026-09-01 12:46:21 +0600

    Update 3d_transformation.c

--- C code/3d_transformation.c
+++ C code/3d_transformation.c
@@ -3,50 +3,84 @@
 #include <stdio.h>
 #include <math.h>
 
-int main() {
-    int gd = DETECT, gm;
-    int x1 = 100, y1 = 100, x2 = 150, y2 = 150, depth = 20;
-    int choice;
+void draw3DBar(int x1, int y1, int x2, int y2, int depth) {
+    bar3d(x1, y1, x2, y2, depth, 1);
+}
 
-    printf("--- 3D Transformation Menu ---\n");
-    printf("1. Translation\n2. Rotation (Z-axis)\n3. Scaling\n4. Reflection (Z-plane)\n5. Shearing (Z-axis)\nChoice: ");
+int main() {
+    int choice;
+    printf("--- 3D Transformations Menu ---\n");
+    printf("1. Translation\n");
+    printf("2. Rotation (Z-axis)\n");
+    printf("3. Scaling\n");
+    printf("4. Reflection (XY Plane)\n");
+    printf("5. Shearing (X-axis relative to Z)\n");
+    printf("Enter choice: ");
     scanf("%d", &choice);
 
+    int x1, y1, x2, y2, depth;
+    printf("Enter x1,y1: ");
+    scanf("%d%d", &x1, &y1);
+
+    printf("Enter x2,y2: ");
+    scanf("%d%d", &x2, &y2);
+
+    printf("Enter depth: ");
+    scanf("%d", &depth);
+
+    int tx, ty, tz;
+    float angle;
+    float sx, sy, sz;
+    float shx;
+
+    if (choice == 1) {
+        printf("Enter tx,ty,tz: ");
+        scanf("%d%d%d", &tx, &ty, &tz);
+    }
+    else if (choice == 2) {
+        printf("Enter angle: ");
+        scanf("%f", &angle);
+    }
+    else if (choice == 3) {
+        printf("Enter sx,sy,sz: ");
+        scanf("%f%f%f", &sx, &sy, &sz);
+    }
+    else if (choice == 5) {
+        printf("Enter shx: ");
+        scanf("%f", &shx);
+    }
+
+    int gd = DETECT, gm;
     initgraph(&gd, &gm, "");
 
-    /* Original 3D Box (White Color) */
     setcolor(WHITE);
-    bar3d(x1, y1, x2, y2, depth, 1);
+    draw3DBar(x1, y1, x2, y2, depth);
 
-    /* Transformed 3D Box (Red Color) */
     setcolor(RED);
 
-    if (choice == 1) { /* 3D Translation */
-        int tx = 80, ty = 50;
-        bar3d(x1 + tx, y1 + ty, x2 + tx, y2 + ty, depth, 1);
-    } 
-    else if (choice == 2) { /* 3D Rotation (Z-axis) */
-        float rad = 30.0 * 3.1416 / 180.0;
-        int nx1 = (int)(x1 * cos(rad) - y1 * sin(rad));
-        int ny1 = (int)(x1 * sin(rad) + y1 * cos(rad));
-        int nx2 = (int)(x2 * cos(rad) - y2 * sin(rad));
-        int ny2 = (int)(x2 * sin(rad) + y2 * cos(rad));
-        
-        bar3d(nx1, ny1, nx2, ny2, depth, 1);
-    } 
-    else if (choice == 3) { /* 3D Scaling */
-        float sx = 1.5, sy = 1.5, sz = 1.5;
-        bar3d((int)(x1 * sx), (int)(y1 * sy), (int)(x2 * sx), (int)(y2 * sy), (int)(depth * sz), 1);
-    } 
-    else if (choice == 4) { /* 3D Reflection */
-        bar3d(x1, -y1 + 300, x2, -y2 + 300, depth, 1);
-    } 
-    else if (choice == 5) { /* 3D Shearing */
-        float shx = 0.5;
-        int nx1 = (int)(x1 + shx * y1);
-        int nx2 = (int)(x2 + shx * y2);
-        
-        bar3d(nx1, y1, nx2, y2, depth, 1);
+    if (choice == 1) {
+        draw3DBar(x1 + tx, y1 + ty, x2 + tx, y2 + ty, depth + tz);
+    }
+    else if (choice == 2) {
+        float rad = angle * 3.1416 / 180;
+        int nx1 = x1 * cos(rad) - y1 * sin(rad);
+        int ny1 = x1 * sin(rad) + y1 * cos(rad);
+        int nx2 = x2 * cos(rad) - y2 * sin(rad);
+        int ny2 = x2 * sin(rad) + y2 * cos(rad);
+
+        draw3DBar(nx1, ny1, nx2, ny2, depth);
+    }
+    else if (choice == 3) {
+        draw3DBar(x1 * sx, y1 * sy, x2 * sx, y2 * sy, depth * sz);
+    }
+    else if (choice == 4) {
+        draw3DBar(x1, y1, x2, y2, -depth);
+    }
+    else if (choice == 5) {
+        int nx1 = x1 + shx * depth;
+        int nx2 = x2 + shx * depth;
+
+        draw3DBar(nx1, y1, nx2, y2, depth);
     }
 
     getch();
