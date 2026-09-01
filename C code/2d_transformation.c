01cc49c6b1fa0dfe15c230023680cd44689d5144
Nilanta Roy Nilay <156948038+Nilanta-Roy-Nilay@users.noreply.github.com>
2026-09-01 12:25:00 +0600

    Update 2d_transformation.c

--- C code/2d_transformation.c
+++ C code/2d_transformation.c
@@ -11,15 +11,6 @@ void drawPolygon(int x1, int y1, int x2, int y2, int x3, int y3, int x4, int y4)
 }
 
 int main() {
-    int gd = DETECT, gm;
-    initgraph(&gd, &gm, "");
-
-    // Original Rectangle Coordinates
-    int x1 = 100, y1 = 100;
-    int x2 = 200, y2 = 100;
-    int x3 = 200, y3 = 200;
-    int x4 = 100, y4 = 200;
-
     int choice;
     printf("--- 2D Transformations Menu ---\n");
     printf("1. Translation\n");
@@ -30,16 +21,38 @@ int main() {
     printf("Enter choice: ");
     scanf("%d", &choice);
 
-    // Original Shape
-    rectangle(x1, y1, x3, y3);
+    int x1, y1, x2, y2, x3, y3, x4, y4;
+    printf("Enter x1 y1: ");
+    scanf("%d%d", &x1, &y1);
 
-    if (choice == 1) { // Translation
-        int tx = 100, ty = 50;
-        rectangle(x1 + tx, y1 + ty, x3 + tx, y3 + ty);
-    } 
-    else if (choice == 2) { // Rotation
-        float angle = 12;
-        float rad = angle * 3.1416 / 180;
+    printf("Enter x2 y2: ");
+    scanf("%d%d", &x2, &y2);
+
+    printf("Enter x3 y3: ");
+    scanf("%d%d", &x3, &y3);
+
+    printf("Enter x4 y4: ");
+    scanf("%d%d", &x4, &y4);
+
+    int gd = DETECT, gm;
+    initgraph(&gd, &gm, "");
+
+    setcolor(WHITE);
+    drawPolygon(x1, y1, x2, y2, x3, y3, x4, y4);
+
+    setcolor(RED);
+
+    if (choice == 1) { 
+        int tx, ty;
+        printf("Enter tx ty: ");
+        scanf("%d%d", &tx, &ty);
+        drawPolygon(x1 + tx, y1 + ty, x2 + tx, y2 + ty, x3 + tx, y3 + ty, x4 + tx, y4 + ty);
+    }
+    else if (choice == 2) { 
+        float angle;
+        printf("Enter angle: ");
+        scanf("%f", &angle);
+        float rad = angle * 3.14159 / 180.0;
 
         int nx1 = x1 * cos(rad) - y1 * sin(rad);
         int ny1 = x1 * sin(rad) + y1 * cos(rad);
@@ -51,17 +64,20 @@ int main() {
         int ny4 = x4 * sin(rad) + y4 * cos(rad);
 
         drawPolygon(nx1, ny1, nx2, ny2, nx3, ny3, nx4, ny4);
-    } 
-    else if (choice == 3) { // Scaling
-        float sx = 1.5, sy = 2.0;
-        rectangle(x1 * sx, y1 * sy, x3 * sx, y3 * sy);
-    } 
-    else if (choice == 4) { // Reflection
-        // Screen center adjustment for Reflection visual
-        rectangle(x1, -y1 + 300, x3, -y3 + 300);
-    } 
-    else if (choice == 5) { // Shearing
-        int shx = 1;
+    }
+    else if (choice == 3) { 
+        float sx, sy;
+        printf("Enter sx sy: ");
+        scanf("%f%f", &sx, &sy);
+        drawPolygon(x1 * sx, y1 * sy, x2 * sx, y2 * sy, x3 * sx, y3 * sy, x4 * sx, y4 * sy);
+    }
+    else if (choice == 4) { 
+        drawPolygon(x1, -y1 + 300, x2, -y2 + 300, x3, -y3 + 300, x4, -y4 + 300);
+    }
+    else if (choice == 5) {
+        int shx;
+        printf("Enter shx: ");
+        scanf("%d", &shx);
         int nx1 = x1 + shx * y1;
         int nx2 = x2 + shx * y2;
         int nx3 = x3 + shx * y3;
